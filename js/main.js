969649b35b7e48bf28d91d4e5ef6f8a5022755f8
SebastianVergaraVelasquez <97114145+SebastianVergaraVelasquez@users.noreply.github.com>
2024-02-09 07:27:19 -0500

    feat: :tada: Inicio glosario

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Tipos de datos
+
+string = "";
+numero = 25;
+bool = true;
+
+// variables
+
+const edad = 25; //Const debe declararse en el momento que se inicializa
+let nombre;
+var cargo;
+
+//otros valores: undefined, null, NaN
+
+//prompt --> Input
+
+const nombreIngresado = prompt("Hola")
+
+alert(nombreIngresado)
+
+console.log("Hola mundo")
